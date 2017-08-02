417648bd0bb484b263249b627f91960fc44b9370
siukola <siukola@example.com>
2017-08-02 12:27:34 +0300

    Identical requests blocked.

--- waitercaller.py
+++ waitercaller.py
@@ -96,8 +96,9 @@ def account_deletetable():
 
 @app.route("/newrequest/<tid>")
 def new_request(tid):
-    DB.add_request(tid, datetime.datetime.now())
-    return "Your request has been logged and a waiter will be with you shortly"
+    if DB.add_request(tid, datetime.datetime.now()):
+        return "Your request has been logged and a waiter will be with you shortly"
+    return "There is already a request pending for this table. Please be patient, a waiter will be there ASAP"
 
 @app.route("/dashboard/resolve")
 @login_required
